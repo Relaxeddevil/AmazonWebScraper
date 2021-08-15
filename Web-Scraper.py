dc9be3e0d088a6c40e3722383e7d5d4eee041c4f
Rayyan Majid <rayyanmajidis@gmail.com>
2021-08-15 04:45:47 -0400

    created function which returns formatted html of the amazon webpage after a search is conducted and filtered by highest reviews

--- Web-Scraper.py
+++ Web-Scraper.py
@@ -1,2 +1,40 @@
-A web-scraper, which scrapes amazon listing given a search parameter
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
 
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                     "Chrome/92.0.4515.131 ""Safari/537.36")
+
+
+driver = webdriver.Chrome(options=options)
+
+
+def get_soup(keyword):
+    url = 'https://www.amazon.ca/'
+    driver.get(url)
+    driver.maximize_window()
+
+    search_box = driver.find_element_by_id("twotabsearchtextbox")
+    search_box.send_keys(keyword)
+    search_box.submit()
+
+    sort = driver.find_element_by_id('a-autoid-0-announce')
+    sort.click()
+
+    av_reviews = driver.find_element_by_id('s-result-sort-select_3')
+    av_reviews.click()
+
+    time.sleep(1)
+    driver.save_screenshot('local_ss.png')
+
+    html = driver.page_source
+
+    soup = BeautifulSoup(html, 'lxml')
+
+    return soup
+
+
+print(get_soup('rope'))
